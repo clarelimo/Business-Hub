d4ed8937ee2c9ec868fd53ef8c89614b010bc387
clarelimo <clare.limo@student.moringaschool.com>
2021-06-30 17:14:35 +0300

    branding styles added

--- landing/js/branding.js
+++ landing/js/branding.js
@@ -1,6 +1,36 @@
-$(document).ready(function(){
-    $("button#offerbtn").click(function(){
-        $("branding-offers").show();
+var brandings = [{name:"Logo",price: 5000}, {name:"Stationery",price: 15000},{name:"Website",price: 30000},{name:"Banner",price: 10000}];
 
+function Brand(name, price){
+    this.name = name;
+    this.price = price;
+}
+
+$(document).ready(function(){
+    $("button").click(function(){
+        var total =0;
+        var selected =[];
+        $.each($("input[name='brandings']:checked"),function(){
+            selected.push($(this).val())
+        });
+
+        brandings.forEach(function(branding){    
+            if(selected.includes(branding.name)){
+                total += branding.price;
+            }
+        })
+        
+        $("#orders-display").show();
+
+        if(selected.length > 0){
+            selected.forEach(function(brand){
+                $("#total-orders").append('<tr><td id="brandingname">'+brand);
+            })  
+            $("#brandingtotalprice").text("The Total Price is: " + total);
+            alert("You have selected " +selected.join(",")+" The total Price is: " + total);
+        }else{
+            alert("Please selected the kinds of brandings you need.")
+        }
+
+        $('input[name="brandings"]').prop('checked', false);
     });
 });
